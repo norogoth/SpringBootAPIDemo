b6ba621e14e4e23460ffc5723362b9d15432cfd2
norogoth <norogoth@gmail.com>
2023-02-08 13:52:44 -0800

    fixing to allow cors for dem ouse

--- src/main/java/com/example/demoAPI/InventoryController.java
+++ src/main/java/com/example/demoAPI/InventoryController.java
@@ -1,9 +1,11 @@
 package com.example.demoAPI;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class InventoryController {
     private InventoryLoader inventoryLoader;
 
